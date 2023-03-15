96695b0fdf912a4de9725617fdb0acaa0b5340b4
Izac Peterson <izacpeterson@gmail.com>
2023-03-14 20:31:24 -0600

    filepath

--- src/routes/pictures/+page.server.js
+++ src/routes/pictures/+page.server.js
@@ -3,7 +3,11 @@ import path from "path";
 import { redirect } from "@sveltejs/kit";
 
 export async function load() {
+  let dir = path.join(process.cwd(), "images");
   let imgs = await fs.readdir("./images");
+  imgs = imgs.map((img) => {
+    console.log(imgs);
+  });
   return { imgs };
 }
 
@@ -11,15 +15,8 @@ export const actions = {
   avatar: async ({ request, route, url }) => {
     try {
       const data = Object.fromEntries(await request.formData());
-      const filePath = path.join(
-        process.cwd(),
-        "images",
-        `${crypto.randomUUID()}.${data.avatar.type.split("/")[1]}`
-      );
-      await fs.writeFile(
-        filePath,
-        Buffer.from(await data.avatar.arrayBuffer())
-      );
+      const filePath = path.join(process.cwd(), "images", `${crypto.randomUUID()}.${data.avatar.type.split("/")[1]}`);
+      await fs.writeFile(filePath, Buffer.from(await data.avatar.arrayBuffer()));
 
       // TODO: store the file path in database for further references.
       throw redirect(303, url.pathname);
